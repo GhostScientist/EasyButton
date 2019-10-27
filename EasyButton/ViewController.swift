1b9cebd8a9edc822da4483bf88a1b43a01c554f1
Dakota Kim <codingvampire@gmail.com>
2019-10-27 18:48:06 -0400

    Initial Commit

---
+++ EasyButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyButton
+//
+//  Created by Dakota Kim on 10/27/19.
+//  Copyright © 2019 Dakota Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
