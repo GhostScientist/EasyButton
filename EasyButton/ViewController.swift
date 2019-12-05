957793b5be61afe84e7fca2b47eb779c01016139
Dakota Kim <codingvampire@gmail.com>
2019-12-05 09:32:59 -0500

    add basic button, title, constraints, and button action

--- EasyButton/ViewController.swift
+++ EasyButton/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var easyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func easyButtonTapped(_ sender: Any) {
+        print("That was easy!")
+    }
+    
 }
 
