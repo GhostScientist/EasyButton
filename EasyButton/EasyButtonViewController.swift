943ea972c3a4f5f86189c0f4a1d26a0058eee0ff
Dakota Kim <codingvampire@gmail.com>
2020-01-24 10:10:10 -0500

    remove storyboard reference, set up root view controller via scene delegate, add easybutton vc

---
+++ EasyButton/EasyButtonViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EasyButtonViewController.swift
+//  EasyButton
+//
+//  Created by Dakota Kim on 1/24/20.
+//  Copyright © 2020 Dakota Kim. All rights reserved.
+//
+
+import UIKit
+
+class EasyButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+}
